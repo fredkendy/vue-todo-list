ccd310878980f689c3c2955e61605262457cada8
fredkendy <fredkendy9@gmail.com>
2023-12-01 17:20:31 -0300

    implementando a funcionalidade de alterar o botao e enviar ajax

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,7 +3,13 @@
     <div class="bg-gray-300 rounded-sm">
           <div class="flex items-center px-4 py-3 border-b border-gray-400 last:border-b-0">
             <div class="flex items-center justify-center mr-2">
-              <button class="text-gray-400">
+              <button 
+                :class="{
+                  'text-green-600': isCompleted,
+                  'text-red-400': !isCompleted
+                }"
+                @click="onCheckClick"
+              >
                 <svg
                   class="w-5 h-5"
                   fill="none"
@@ -24,11 +30,11 @@
             <div class="w-full">
               <!-- v-on:keyup.enter (quando soltar o enter...) -->
               <input
+                v-model="title"
                 type="text"
                 placeholder="Digite a sua tarefa"
-                :value="todo.title"
                 class="bg-gray-300 placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal mr-3" 
-                @keyup.enter="updateTodo"
+                @keyup.enter="onTitleChange"
               />
             </div>
 
@@ -63,23 +69,44 @@ export default {
       default: () => ({}),
     },
   },
+
+  data() {
+    return {
+      //declarando 2 variáveis, para não mexer direto nas props
+      title: this.todo.title,
+      isCompleted: this.todo.completed
+    }
+  },
+
   methods: {
-    updateTodo(e) {
-      //pegando o título
-      const newTitle = e.target.value
-      if (!newTitle) {
+    //Quando apertar enter
+    onTitleChange() {
+      if (!this.title) {
         return
       }
 
+      this.updateTodo()
+    },
+
+    //Empurra pro back-end através do $store do vuex
+    updateTodo() {
       //'montando' o json  
       const payload = {
         id: this.todo.id,
         data: {
-          title: newTitle,
-          completed: this.todo.completed
+          title: this.title,
+          completed: this.isCompleted
         }
       }
       this.$store.dispatch('updateTodo', payload)
+    },
+
+    //Quando o check for clicado
+    onCheckClick() {
+      //toggle do botão (apenas visual local)
+      this.isCompleted = !this.completed
+      //fazer ajax para de fato alterar
+      this.updateTodo() 
     }
   }
 }

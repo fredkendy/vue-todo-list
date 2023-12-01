69abe047b198e2dd28b11ab91e41c3e2e4a4e261
fredkendy <fredkendy9@gmail.com>
2023-12-01 15:01:52 -0300

    componentizacao da aplicacao

---
+++ src/components/TodoEmpty.vue
@@ -0,0 +1,6 @@
+<!-- Todo empty: Estado de vazio, caso não tenha nenhuma tarefa -->
+<template>
+    <div class="text-center text-lg text-gray-500">
+        Você ainda não tem nenhuma tarefa.
+    </div>
+</template>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,19 @@
+<!-- Representa o agrupamento dos items -->
+<template>
+    <!-- Todo items (tarefa unitária) -->
+    <div class="space-y-2">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+export default {
+    components: {
+        TodoItem
+    }
+}
+</script>

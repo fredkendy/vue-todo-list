dddba183b0eb678539a164b78152b96fc3f24a29
fredkendy <fredkendy9@gmail.com>
2023-12-01 14:12:32 -0300

    setup inicial vue + instalacao tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,vue}",
+    //Indica que irá aplicar o tailwind nesse arquivo
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

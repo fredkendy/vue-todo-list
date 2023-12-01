fa39a954aa0e56fda266ba10985a6dc2cec28fc8
fredkendy <fredkendy9@gmail.com>
2023-12-01 15:12:39 -0300

    fazendo requisicoes com o axios

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
   <div class="px-3 py-10 md:px-10">
     <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
 
+      <pre>
+        {{ todos }}
+      </pre>
+
       <TodoSpinner />
 
       <TodoForm />
@@ -20,9 +24,27 @@ import TodoSpinner from './components/TodoSpinner.vue';
 import TodoForm from './components/TodoForm.vue';
 import TodoItems from './components/TodoItems.vue';
 import TodoEmpty from './components/TodoEmpty.vue';
+import axios from 'axios';
 
 export default {
     name: "App",
-    components: { TodoSpinner, TodoForm, TodoItems, TodoEmpty }
+    components: { 
+      TodoSpinner, TodoForm, TodoItems, TodoEmpty 
+    },
+
+    data() {
+      return {
+        todos: []
+      }
+    },
+
+    created() {
+      //Requisição ao json-server (database.json) com as todos
+      //então, atribuir o resultado ao array declarado no data() (poderia ter utilizado o async await)
+      axios.get('http://localhost:3000/todos')
+        .then((response) => {
+          this.todos = response.data
+        })
+    }
 };
 </script>
